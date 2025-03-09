dcfb04e41cce859f5133755dbe1829483df60e6e
jesperkha <hammerjesp@gmail.com>
2025-03-09 20:49:46 +0100

    Move main to app and make cenv pkg top level

---
+++ cenv.go
@@ -0,0 +1,7 @@
+package cenv
+
+import "github.com/echo-webkom/cenv/cenv"
+
+func CheckAndLoad() error {
+	return cenv.CheckAndLoad()
+}
---
+++ cenv/pkg.go
@@ -0,0 +1,11 @@
+package cenv
+
+const (
+	defaultEnvPath    = ".env"
+	defaultSchemaPath = "cenv.schema.json"
+)
+
+func CheckAndLoad() error {
+
+	return nil
+}
