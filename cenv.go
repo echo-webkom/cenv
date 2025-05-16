de100f1575504562a23ec6850a03f44a39b3f264
jesperkha <hammerjesp@gmail.com>
2025-05-16 15:04:14 +0200

    Make function to verify existing env

--- cenv.go
+++ cenv.go
@@ -14,9 +14,14 @@ const (
 // The default path for both .env and cenv.schema.json are used, which are
 // both in the project root directory. To specify path use LoadEx()
 func Load() error {
-	return internal.LoadAndCheck(defaultEnvPath, defaultSchemaPath)
+	return internal.LoadAndCheck(defaultEnvPath, defaultSchemaPath, false)
 }
 
 func LoadEx(envPath string, schemaPath string) error {
-	return internal.LoadAndCheck(envPath, schemaPath)
+	return internal.LoadAndCheck(envPath, schemaPath, false)
+}
+
+// Verify that the values in the loaded environment mathces the schema.
+func Verify() error {
+	return internal.LoadAndCheck(defaultEnvPath, defaultSchemaPath, true)
 }
