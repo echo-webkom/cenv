eb2e857d05e43bfcc5b0eed09958d96c3aab55eb
jesperkha <hammerjesp@gmail.com>
2025-05-26 13:55:30 +0200

    Check version only every 24 hours
    Check version only every 24 hours

--- cmd/version.go
+++ cmd/version.go
@@ -2,9 +2,13 @@ package cmd
 
 import (
 	"encoding/json"
+	"io"
+	"log"
 	"net/http"
 	"os"
+	"path"
 	"strings"
+	"time"
 
 	"slices"
 
@@ -20,6 +24,10 @@ func checkIfLatestVersion() {
 	}
 
 	if Version == "" {
+		return
+	}
+
+	if !shouldCheck() {
 		return
 	}
 
@@ -47,4 +55,38 @@ func checkIfLatestVersion() {
 	if !isLatest {
 		color.Yellow("A new version of cenv is available. Run 'cenv upgrade' to upgrade to the latest version.")
 	}
+}
+
+// cenv creates the file .cenv-version in your home dir, which contains
+// the last time for version check. If the file does not exist or the the time
+// is past expiration, cenv checks again. This is to minimize delay when running
+// cenv as checking for an update may be slow and annoying on slower internet.
+func shouldCheck() bool {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	filepath := path.Join(home, ".cenv-version")
+
+	file, err := os.OpenFile(filepath, os.O_RDWR|os.O_CREATE, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+	b, err := io.ReadAll(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	expiration := time.Hour * 24
+	checked, err := time.Parse(time.RFC3339, string(b))
+	if err != nil || time.Now().Sub(checked) > expiration {
+		file.Seek(0, io.SeekStart)
+		file.WriteString(time.Now().Format(time.RFC3339))
+		return true
+	}
+
+	return false
 }
